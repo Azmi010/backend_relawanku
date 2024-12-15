835c5803141352c99f456b634b5b54b5ee4a007a
lyeiva86 <ulul10039@gmail.com>
2024-12-15 23:46:04 +0700

    fix: feature donasi

--- repository/auth/user_record.go
+++ repository/auth/user_record.go
@@ -15,17 +15,17 @@ const (
 )
 
 type User struct {
-	ID        uint           `gorm:"primaryKey"`
-	Username  string         `gorm:"unique;not null" json:"username" form:"username"`
-	Email     string         `gorm:"email;not null" json:"email" form:"email"`
-	Password  string         `gorm:"not null" json:"password" form:"password"`
-	Gender    string         `json:"gender" form:"gender"`
-	Address   string         `json:"address" form:"address"`
+	ID           uint                `gorm:"primaryKey"`
+	Username     string              `gorm:"unique;not null" json:"username" form:"username"`
+	Email        string              `gorm:"email;not null" json:"email" form:"email"`
+	Password     string              `gorm:"not null" json:"password" form:"password"`
+	Gender       string              `json:"gender" form:"gender"`
+	Address      string              `json:"address" form:"address"`
 	Role         UserRole            `gorm:"not null" json:"role"`
-	ImageUrl  string         `json:"image_url" form:"image_url"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+	ImageUrl     string              `json:"image_url" form:"image_url"`
+	CreatedAt    time.Time           `json:"created_at"`
+	UpdatedAt    time.Time           `json:"updated_at"`
+	DeletedAt    gorm.DeletedAt      `gorm:"index" json:"deleted_at,omitempty"`
 	Transactions []model.Transaction `gorm:"foreignKey:UserID" json:"user_id"`
 }
 
